a867993ceaf7dc7d10f4a1ff6dcf45292ba5aca6
flashgrinder <web@fcdigital.ru>
2019-10-21 15:34:45 +0400

    Сверстал форму обратной связи и форму акций

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,22 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
 	let burger = document.querySelector('.headerTop__burger');
 	let menu = document.querySelector('.headerMenu');
 	let link = document.querySelectorAll('.headerMenu__navigation > ul > li > a');
 
-	function showMenu(){
+	function showMenu() {
 		menu.classList.toggle('headerMenu_show');
 	}
 
 	burger.addEventListener('click', showMenu);
 
 	for (var i = 0; i < link.length; i++) {
-		link[i].addEventListener('click', function() {
+		link[i].addEventListener('click', function () {
 			menu.classList.remove('headerMenu_show');
 		});
 	}
 
-	var mySwiper1 = new Swiper ('.slider_one', {
+	// Popups
+	let btnServices = document.querySelector('#popaps-form');
+	let modal = document.querySelector('.formPopaps');
+	let formBg = document.querySelector('.form-bg');
+
+	function showModal() {
+		modal.classList.add('formPopaps_active');
+		formContainer.classList.add('form-bg_active')
+	}
+	function hideModal() {
+		modal.classList.remove('formPopaps_active');
+		formContainer.classList.remove('form-bg_active')
+	}
+
+	btnServices.addEventListener('click', showModal);
+	formBg.addEventListener('click', hideModal)
+
+	// Sliders
+	var mySwiper1 = new Swiper('.slider_one', {
 		// Optional parameters
 		direction: 'horizontal',
 		loop: true,
@@ -28,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			delay: 3000,
 		},
 	});
-	var mySwiper2 = new Swiper ('.slider_two', {
+	var mySwiper2 = new Swiper('.slider_two', {
 		// Optional parameters
 		direction: 'horizontal',
 		loop: true,
@@ -40,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			delay: 3000,
 		}
 	});
-	var mySwiper3 = new Swiper ('.slider_three', {
+	var mySwiper3 = new Swiper('.slider_three', {
 		// Optional parameters
 		direction: 'horizontal',
 		loop: true,
